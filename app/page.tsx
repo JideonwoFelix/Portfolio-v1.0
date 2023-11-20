279fd8f283006e80dd3467d3c92392d2c5400b7b
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2023-11-20 07:44:44 +0100

    initial comit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-32 p-24 text-center">
+      <div className='text-5xl md:text-6xl'>
+        Jideonwo Felix
+        <div className='text-sm md:text-2xl text-[#c1c1c1]'>Web Dev, Computer Harware Technician</div>
+        </div>
+      <div className='text-4xl md:text-6xl text-[#444]'>Comming Soon...</div>
+    </main>
+  )
+}
